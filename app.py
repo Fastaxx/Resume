780c355b645ba40261115137b96db11707649eb7
Fastaxx <Fastaxx@github.com>
2022-02-24 18:59:32 +0100

    suite

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ import streamlit as st
 import pages.edu, pages.projets, pages.exp, pages.skills, pages.propos
 from PIL import Image
 import ressources.ast as ast
+hide_menu = """ 
+<style>
+#MainMenu {
+	visibility:hidden;
+}
 
+footer {
+	visibility:hidden;
+}@
+</style>
+"""
 PAGES = {
     "🙋 Introduction": pages.propos,
     "👨‍💻️ Professional experience": pages.exp,
@@ -11,8 +21,22 @@ PAGES = {
     "🎓 Education" : pages.edu,
 }
 
+PAGE_CONFIG = {"page_title" : "Resume Louis Libat",
+"page_icon":"https://img.icons8.com/ios/50/000000/resume.png",
+"layout":"centered",
+"initial_sidebar_state":"expanded",
+"menu_items":{
+	'Get Help': None,
+	'Report a bug':None,
+	'About': "Test"
+}}
+
+st.set_page_config(**PAGE_CONFIG)
+
+
 image = Image.open('img/louis.jpeg') 
 def main():
+	st.markdown(hide_menu,unsafe_allow_html=True)
 	selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 	page = PAGES[selection]
 	with st.spinner(f"Loading {selection} ..."):
